b5d4daa6d2a9dd94b4f5a9a2993e9f3a4e34d438
Kayfresh <madubuike1573080@gmail.com>
2024-11-20 12:49:27 +0100

    Edited text in the paragraphs and made only one faq to be open at a time

--- Javascript/script.js
+++ Javascript/script.js
@@ -17,24 +17,72 @@ expandOne.addEventListener("click", () => {
   paragraphOne.style.display = "block";
   contractOne.style.display = "block";
   expandOne.style.display = "none";
+
+  paragraphTwo.style.display = "none";
+  expandTwo.style.display = "block";
+  contractTwo.style.display = "none";
+
+  paragraphThree.style.display = "none";
+  expandThree.style.display = "block";
+  contractThree.style.display = "none";
+
+  paragraphFour.style.display = "none";
+  expandFour.style.display = "block";
+  contractFour.style.display = "none";
 });
 
 expandTwo.addEventListener("click", () => {
   paragraphTwo.style.display = "block";
   contractTwo.style.display = "block";
   expandTwo.style.display = "none";
+
+  paragraphOne.style.display = "none";
+  expandOne.style.display = "block";
+  contractOne.style.display = "none";
+
+  paragraphThree.style.display = "none";
+  expandThree.style.display = "block";
+  contractThree.style.display = "none";
+
+  paragraphFour.style.display = "none";
+  expandFour.style.display = "block";
+  contractFour.style.display = "none";
 });
 
 expandThree.addEventListener("click", () => {
   paragraphThree.style.display = "block";
   contractThree.style.display = "block";
   expandThree.style.display = "none";
+
+  paragraphOne.style.display = "none";
+  expandOne.style.display = "block";
+  contractOne.style.display = "none";
+
+  paragraphTwo.style.display = "none";
+  expandTwo.style.display = "block";
+  contractTwo.style.display = "none";
+
+  paragraphFour.style.display = "none";
+  expandFour.style.display = "block";
+  contractFour.style.display = "none";
 });
 
 expandFour.addEventListener("click", () => {
   paragraphFour.style.display = "block";
   contractFour.style.display = "block";
   expandFour.style.display = "none";
+
+  paragraphOne.style.display = "none";
+  expandOne.style.display = "block";
+  contractOne.style.display = "none";
+
+  paragraphTwo.style.display = "none";
+  expandTwo.style.display = "block";
+  contractTwo.style.display = "none";
+
+  paragraphThree.style.display = "none";
+  expandThree.style.display = "block";
+  contractThree.style.display = "none";
 });
 
 // Closing the Paragraphs
